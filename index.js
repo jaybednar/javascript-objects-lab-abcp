6698f71f7600fc9fe11eae23816ff5d21589bd4b
Jay Bednar <ironviralmedia@gmail.com>
2018-01-12 15:28:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  return delete object.key;
+  object = delete object.key;
+  return object;
 }
